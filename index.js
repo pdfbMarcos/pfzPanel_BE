1df59425e3256aa683ea8f8892e14788dfd9c0a2
pdfbMarcos <109549423+pdfbMarcos@users.noreply.github.com>
2023-09-27 14:20:15 +0100

    Add files via upload
    Mais ajustes para resolver erro CORS

--- index.js
+++ index.js
@@ -8,15 +8,7 @@ const app = express();
 app.use(express.json(), (_req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    POST,
-    PUT,
-    PATCH,
-    GET,
-    DELETE,
-    OPTIONS
-  );
+  res.header("Access-Control-Allow-Methods", "*");
 
   next();
 });
