52db53bd46189d876294b37539de3ff54d7e2440
pdfbMarcos <109549423+pdfbMarcos@users.noreply.github.com>
2023-09-27 11:59:21 +0100

    Add files via upload
    Incluindo Access-Control-Allow

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const app = express();
 app.use(express.json(), (_req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    POST,
+    PUT,
+    PATCH,
+    GET,
+    DELETE,
+    OPTIONS
+  );
 
   next();
 });
